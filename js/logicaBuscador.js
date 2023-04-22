46c2faafe6b3baec4bb4701f00fd2d83fca7511c
roxania <groxania@gmail.com>
2023-04-22 09:59:49 -0300

    buscador actulizado

---
+++ js/logicaBuscador.js
@@ -0,0 +1,62 @@
+function searchFunction() {
+    let input, filter, cards, cardContainer, title, i;
+    input = document.getElementsByName("search")[0];
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("seccionDestino");
+    cards = cardContainer.getElementsByClassName("cardTexto");
+    let encontrado = false;
+
+    // Guarda el orden original de las cartas si no se ha guardado aún
+    if (!cardContainer.originalOrder) {
+        cardContainer.originalOrder = [];
+        for (i = 0; i < cards.length; i++) {
+            cardContainer.originalOrder.push(cards[i]);
+        }
+    }
+
+    if (filter === "") { // Si no hay término de búsqueda, restablece el estilo y el orden de las cartas
+        for (i = 0; i < cards.length; i++) {
+            cards[i].style.display = "";
+            cards[i].style.border = "";
+            cardContainer.appendChild(cardContainer.originalOrder[i]);
+        }
+    } else {
+        for (i = 0; i < cards.length; i++) {
+            title = cards[i].querySelector(".cardTexto h4");
+            if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+                cards[i].style.display = "";
+                cards[i].style.border = "2px solid red";
+                cardContainer.insertBefore(cards[i], cardContainer.firstChild);
+                encontrado = true;
+            } else {
+                cards[i].style.display = "none";
+            }
+        }
+    }
+
+    if (!encontrado && filter !== "") {
+        let noResults = document.createElement("p");
+        noResults.innerText = "No se encontró ningún resultado para su búsqueda.";
+        noResults.setAttribute("id", "resultado");
+        // verificar si el mensaje ya está presente
+        if (!cardContainer.querySelector("#resultado")) {
+            cardContainer.appendChild(noResults);
+        }
+    } else {
+
+        let noResults = cardContainer.querySelector("#resultado");
+        if (noResults && cardContainer.contains(noResults)) {
+            cardContainer.removeChild(noResults);
+
+        }
+        if (filter === "") { // Si no hay término de búsqueda, restablece el estilo y el orden de las cartas
+            for (i = 0; i < cards.length; i++) {
+                cards[i].style.display = "";
+                cards[i].style.border = "";
+                cardContainer.appendChild(cardContainer.originalOrder[i]);
+            }
+        }
+
+    }
+}
+
